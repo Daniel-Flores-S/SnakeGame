73a004bd5f78b3f327f5385c6c3c92de6bded666
Daniel <danielfloresda802@gmail.com>
2021-02-07 11:10:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //....
+let box = 32;
+
+function createSnake (){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+createSnake();
